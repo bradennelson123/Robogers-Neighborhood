55b4a60b599692df5a5820ac3fa9d0cc285af93e
Braden Nelson <connornel123@outlook.com>
2021-06-11 12:22:53 -0700

    add new form features, incomplete function

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+
+// Business Logic
+
+// create array
+let numberValue = [];
+
+const addNumber = (ev)=> {
+ev.preventDefault(); // stop the form from submitting
+let numberValue = {
+getElementById('number)'.value
+}
+}
+
+
+
+
+
+
+
+
+
+
+// UI Logic 
+
